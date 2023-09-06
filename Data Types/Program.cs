e5979cf4caddd76be3582d544d36d4613c1abf7a
Dina Latypova <dinaobnaglela@yandex.ru>
2023-09-06 11:57:56 +0300

    Демонстрация того, что переменные должны быть проинициализированы

--- Data Types/Program.cs
+++ Data Types/Program.cs
@@ -4,14 +4,29 @@ namespace Data_Types
 {
     internal class Program
     {
-        static void DoSomething()
-        {
-            int result;
-            Console.WriteLine(result);
+        //static void DoSomething()
+        //{
+        //    int result;
+        //    Console.WriteLine(result);
 
-        }
+        //}
         static void Main(string[] args)
         {
+            //Неявно типизированные переменные - тип определяется с помощью инициализатора
+            var name = "Дина";
+            var age = 26;
+            var isProgrammer = true;
+
+            // Получить тип переменных
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+
+            Console.WriteLine("Тип name: {0}", nameType);
+            Console.WriteLine($"Тип age {ageType}");
+            Console.WriteLine("Тип isProgrammer {0}", isProgrammer.GetType());
+
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
         }
     }
 }
