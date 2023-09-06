d3b2da56296286858df44c24385d566141042983
Dina Latypova <dinaobnaglela@yandex.ru>
2023-09-06 11:48:04 +0300

    Добавьте файлы проекта.

---
+++ Data Types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void DoSomething()
+        {
+            int result;
+            Console.WriteLine(result);
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
